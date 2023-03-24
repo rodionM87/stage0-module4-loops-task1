2907fe1c6b1f1d871b06a0de59a267ecde199284
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-24 12:12:14 +0600

    Update RepeatIsBad.java

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,8 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        for(int i=1; i<=20; i++) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }
     }
 }
